3ccb8f1dd0ec429a1af0f7a80d734580ac86cc5c
jnjna <jinaregmi025@gmail.com>
2026-07-10 23:22:17 +0800

    Implement my_strcmp using pointer comparison

--- string-library/include/mystring.h
+++ string-library/include/mystring.h
@@ -3,5 +3,6 @@
 
 int my_strlen(const char *str);
 char *my_strcpy(char *dest, const char *str);
+int my_strcmp(const char *str1, const char *str2);
 
 #endif
---
+++ string-library/src/my_strcmp.c
@@ -0,0 +1,10 @@
+#include "mystring.h"
+int my_strcmp(const char *str1, const char *str2){
+    while(*str1!='\0' && *str2!='\0'){
+        if (*str1 != *str2)
+            return *str1 - *str2;
+        str1++;
+        str2++;
+        }
+    return *str1-*str2;
+}
