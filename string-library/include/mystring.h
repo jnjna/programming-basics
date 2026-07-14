4a14b4f6f0c2840cb388996e2878c26a5cb632a9
jnjna <jinaregmi025@gmail.com>
2026-07-14 22:03:23 +0800

    Add custom memory manipulation functions

--- string-library/include/mystring.h
+++ string-library/include/mystring.h
@@ -1,11 +1,22 @@
 #ifndef MYSTRING_H
 #define MYSTRING_H
 
+#include <stddef.h>
+
 int my_strlen(const char *str);
-char *my_strcpy(char *dest, const char *str);
+
+char *my_strcpy(char *dest, const char *src);
+
 int my_strcmp(const char *str1, const char *str2);
-char *my_strcat(char *dest,const char *src);
+
+char *my_strcat(char *dest, const char *src);
+
 char *my_strchr(const char *str, int c);
+
+void *my_memcpy(void *dest, const void *src, size_t n);
+
 void *my_memmove(void *dest, const void *src, size_t n);
+
+void *my_memset(void *ptr, int value, size_t n);
 
 #endif
--- string-library/tests/test_string.c
+++ string-library/tests/test_string.c
@@ -5,9 +5,21 @@ int main(void)
 {
     int length = my_strlen("Hello");
 
-    printf("Testing my_strlen(\"Hello\")\n");
-    printf("Expected: 5\n");
-    printf("Actual:   %d\n", length);
+    printf("Testing my_strlen \n");
 
+    printf("Testing my_strcpy \n");
+
+    printf("Testing my_strcmp \n");
+
+    printf("Testing my_strcat \n");
+
+    printf("Testing my_strchr \n");
+
+    printf("Testing my_memcpy \n");
+
+    printf("Testing my_memmove \n");
+
+    printf("Testing my_memset \n");
+    
     return 0;
 }
